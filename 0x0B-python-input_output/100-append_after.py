e8c03c83d784747f3cecdc58d3e3d74af26d78ca
opegol <opgbenga@gmail.com>
2024-01-10 04:49:22 +0000

    Some advanced tasks in 0x0B-python-input_output project

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text to a file, after each
+    line containing a specific string.
+
+    Args:
+        filename (str):.
+        search_string (str):.
+        new_string (str):.
+    """
+    mod_text = ""
+    with open(filename) as f:
+        for line in f:
+            mod_text += line
+            if search_string in line:
+                mod_text += new_string
+    with open(filename, "w") as wf:
+        wf.write(mod_text)
