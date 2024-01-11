7387f6ea602cb73fc1b86d4a5ca18edfab308c0d
opegol <opgbenga@gmail.com>
2024-01-11 01:04:29 +0000

    Style edits to 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,10 +2,9 @@
 
 """MyList class definition inherits from List."""
 
+
 class MyList(list):
     """ MyList class inherits from list."""
-    
     def print_sorted(self):
         """Prints the list, but sorted (ascending sort)."""
         print(sorted(self))
-
