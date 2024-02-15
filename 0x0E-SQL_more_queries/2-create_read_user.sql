256250c0cf1f4b6a9b659046f810e22773e4ff21
opegol <opgbenga@gmail.com>
2024-02-15 02:01:34 +0000

    Additional edits2 to task2 - more queries project

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,8 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2
 
 -- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
-DROP DATABASE IF EXISTS 'hbtn_0d_2';
-CREATE DATABASE 'hbtn_0d_2';
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+DROP DATABASE IF EXISTS hbtn_0d_2;
+CREATE DATABASE hbtn_0d_2;
+DROP USER IF EXISTS user_0d_2@localhost;
+CREATE USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
